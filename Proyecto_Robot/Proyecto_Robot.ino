8f7735ddf03c5ef3d7b9ee7f995d87047d35c80c
Manuel Díaz Gil <FrkWTF@gmail.com>
2018-12-26 19:09:32 +0100

    Algoritmo Jose

--- Proyecto_Robot/Proyecto_Robot.ino
+++ Proyecto_Robot/Proyecto_Robot.ino
@@ -1,4 +1,13 @@
+#include <StackArray.h>
 #include "movROBOT.h"
+
+
+//Definir matriz de laberinto
+int m[5][5];
+bool s=false;
+StackArray<int> pila;
+
+
 //Definir potencia pila
 int analogPin = 6;
 int potensia = 0;
@@ -56,6 +65,83 @@ float distancia;
 /******************
  *  Funciones     *
  ******************/
+ //Funcion que devuelve true si esta en la casilla negra de la solucion
+ // o devuelve false si no lo esta
+bool solucion(){
+  
+}
+
+bool laberinto(){
+    if(solucion()){
+      s = true;
+    }else{
+      if(!infrarojo_R()){
+        //movemos a la derecha
+        moverDer(500);
+        moverAdelante();
+        pila.push(2);
+        laberinto();
+      }
+      if(!infrarojo_L()){
+        //movemos a la derecha
+        moverIzq(500);
+        moverAdelante();
+        pila.push(1);
+        laberinto();
+      }
+      if(!ultrasonido()){
+        moverAdelante();
+        pila.push(4);
+        laberinto();
+      }
+
+      switch(pila.peek()){
+        case 1:
+          moverAtras();
+          moverIzq(500);
+          break;
+        case 2:
+          moverAtras();
+          moverDer(500);
+          break;
+        case 4:
+          moverAtras();
+          break;
+      }
+
+      pila.pop();
+    }
+    delay(2000);
+}
+
+void salirLaberinto(){
+  while(!pila.isEmpty()){
+    switch(pila.peek()){
+        case 1:
+          moverAtras();
+          moverIzq(500);
+          break;
+        case 2:
+          moverAtras();
+          moverDer(500);
+          break;
+        case 4:
+          moverAtras();
+          break;
+      }
+      pila.pop();
+      delay(2000);
+  }
+}
+ 
+ void iniciarmatriz(){
+  for(int i=0;i<5;i++){
+    for(int j=0;j<5;j++){
+    m[i][j] = 0;
+    }  
+  }
+  s=false;  
+}
  
 void evitarParedI()
 {
@@ -133,7 +219,7 @@ void Calibrar_CNY(){
     Serial1.println(cali_CNY);
 }
 
-void infrarojo_L(){
+bool infrarojo_L(){
     Value_L_Pin=analogRead(L_Pin);
     
     // Calculates the equivalent voltage
@@ -141,13 +227,6 @@ void infrarojo_L(){
     Serial1.print (" Voltage_L: ");
     Serial1.println (Voltage_L);
     delay(1000);
-    if(maxV_detectarpared < Voltage_L)
-    {   
-      Serial1.println("Detecto una pared a mi izquierda, a una distancia de: ");
-      distancia = 11,6 / (Voltage_L - 0,1316);
-      Serial1.println(distancia);
-      
-    }
     
     if(maxV_cercadepared < Voltage_L)
     {   
@@ -156,31 +235,66 @@ void infrarojo_L(){
       Serial1.println(distancia);
       evitarParedI();
     }
+    
+    if(maxV_detectarpared < Voltage_L)
+    {   
+      Serial1.println("Detecto una pared a mi izquierda, a una distancia de: ");
+      distancia = 11,6 / (Voltage_L - 0,1316);
+      Serial1.println(distancia);
+      return true;
+    }
+
+    return false;
+    
 }
 
-void infrarojo_R(){
+bool infrarojo_R(){
     Value_R_Pin=analogRead(R_Pin);
     
     // Calculates the equivalent voltage
     Voltage_R=Value_R_Pin*ResolutionADC;
-    
-    
-    if(maxV_detectarpared < Voltage_R)
-    { 
-      Serial1.println("Detecto una pared a mi derecha, a una distancia de: ");
-       distancia = 11,6 / (Voltage_R - 0,1316);
-      Serial1.println(distancia);
-     
-    }
-    
-    
+
     if(maxV_cercadepared < Voltage_R)
     { 
       Serial1.println("Pared Cerca Derecha, distancia: ");
        distancia = 11,6 / (Voltage_R - 0,1316);
       Serial1.println(distancia);
      evitarParedD();
-    }  
+    }
+    
+    if(maxV_detectarpared < Voltage_R)
+    { 
+      Serial1.println("Detecto una pared a mi derecha, a una distancia de: ");
+       distancia = 11,6 / (Voltage_R - 0,1316);
+      Serial1.println(distancia);
+      return true;
+    }
+
+    return false;
+     
+}
+
+bool ultrasonido(){
+   digitalWrite(Sharp_Pin_Trig, LOW);
+   delayMicroseconds(5);
+  
+  
+   // Send a pulse HIGH for 10 us (According to datasheet) 
+   digitalWrite(Sharp_Pin_Trig, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(Sharp_Pin_Trig, LOW);
+   time_bounce = pulseIn(Sharp_Pin_Echo, HIGH);
+   distance = 0.017 * time_bounce; //Fórmula para calcular la distancia
+   Serial1.println();
+   Serial1.print(distance);
+   Serial1.println(" cm");
+   delay(1000);
+
+   if(distance<=4){
+    Serial1.println("detecto pared alante");
+    return true;
+   }
+   return false;
 }
 
 void setup()
